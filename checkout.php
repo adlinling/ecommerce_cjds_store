3e40f980d4c8928c1f3bded6595d057c50f2a924
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2023-08-12 12:03:43 -0500

    Refinements

--- checkout.php
+++ checkout.php
@@ -5,7 +5,7 @@
 /*
 Paypal button parameters/variables 2019 https://developer.paypal.com/docs/checkout/integrate/#4-set-up-the-transaction (click on the "Orders Create" link).
 
-The paypal code below are taken from the PayPalCheckoutServerSideV2_php.zip package found on https://demo.paypal.com/us/demo/code_samples under the PHP link in the section PayPal Checkout Server-side using Orders v2 APIs and PayPal JavaScript SDK
+The paypal code below is a heavily modified version of the original that was taken from the PayPalCheckoutServerSideV2_php.zip package found on https://demo.paypal.com/us/demo/code_samples under the PHP link in the section PayPal Checkout Server-side using Orders v2 APIs and PayPal JavaScript SDK
 
 
 
@@ -109,9 +109,9 @@ else
 </div>
 
 <br/>
-<div style='display:flex;flex-direction: row;justify-content:center;background-color:#252525;padding:10px;'>
+<div class="checkoutpg-container">
 
-<div style='flex:2;padding:0px;background-color:#252525;'>
+<div class="totalstable">
 
 		<div class='orders-container' >
 		<div class="order-item"></div>
@@ -226,6 +226,7 @@ foreach($_POST['items'] as $cartitem){
     $rootPath = "../";
     include_once('api/Config/Config.php');
     //include('../templates/header.php');
+
 
 
 if(count($_POST)){
@@ -344,7 +345,6 @@ $productsjsonurl = urlencode($productsjson);
 
     //echo "rootpath + services ordercapture: ".URL['services']['orderCapture'];
 
-
 $shippingoptions = $decodedData;
 
 
@@ -360,10 +360,13 @@ $shippingoptions = $decodedData;
     <div class="col-md-offset-4 col-md-4">
         <h3 class="text-center">Billing Information</h3>
         <hr>
+	<form class="form-horizontal" name="myForm" method="POST" action="https://hypcty.com/stripe/public/checkout.php" onsubmit="return validate('<?php echo $reference_id;?>', '<?php echo $ip;?>');">		
 
-	<form class="form-horizontal" name="myForm" method="POST" action="https://hypcty.com/stripe/public/checkout.php" onsubmit="return validate('<?php echo $reference_id;?>', '<?php echo $ip;?>');">
+
 	<?php
-	//<form class="form-horizontal" name="myForm" method="POST" action="http://localhost:4242/checkout.php">
+	//<form class="form-horizontal" name="myForm" method="POST" action="http://localhost:4242/checkout.php">	
+	//<form class="form-horizontal" name="myForm" method="POST" action="stripe/composer/public/checkout.php">
+
 
 
 	foreach($_POST['items'] as $itemvalue){
@@ -484,260 +487,8 @@ $shippingoptions = $decodedData;
                 <div class="col-sm-7">
                     <select class="form-control" name="countrySelectb" id="countrySelectb" onChange='changeshipping("bill", "<?php echo $productsjsonurl;?>");' >
 			<?php
-			$countries = array(
-				"US" => "United States of America (the)",
-				"AF" => "Afghanistan",
-				"AX" => "Åland Islands",
-				"AL" => "Albania",
-				"DZ" => "Algeria",
-				"AS" => "American Samoa",
-				"AD" => "Andorra",
-				"AO" => "Angola",
-				"AI" => "Anguilla",
-				"AQ" => "Antarctica",
-				"AG" => "Antigua and Barbuda",
-				"AR" => "Argentina",
-				"AM" => "Armenia",
-				"AW" => "Aruba",
-				"AU" => "Australia",
-				"AT" => "Austria",
-				"AZ" => "Azerbaijan",
-				"BS" => "Bahamas(the)",
-				"BH" => "Bahrain",
-				"BD" => "Bangladesh",
-				"BB" => "Barbados",
-				"BY" => "Belarus",
-				"BE" => "Belgium",
-				"BZ" => "Belize",
-				"BJ" => "Benin",
-				"BM" => "Bermuda",
-				"BT" => "Bhutan",
-				"BO" => "Bolivia (Plurinational State of)",
-				"BQ" => "Bonaire, Sint Eustatius and Saba",
-				"BA" => "Bosnia and Herzegovina",
-				"BW" => "Botswana",
-				"BV" => "Bouvet Island",
-				"BR" => "Brazil",
-				"IO" => "British Indian Ocean Territory (the)",
-				"BN" => "Brunei Darussalam",
-				"BG" => "Bulgaria",
-				"BF" => "Burkina Faso",
-				"BI" => "Burundi",
-				"CV" => "Cabo Verde",
-				"KH" => "Cambodia",
-				"CM" => "Cameroon",
-				"CA" => "Canada",
-				"KY" => "Cayman Islands (the)",
-				"CF" => "Central African Republic (the)",
-				"TD" => "Chad",
-				"CL" => "Chile",
-				"CN" => "China",
-				"CX" => "Christmas Island",
-				"CC" => "Cocos (Keeling) Islands(the)",
-				"CO" => "Colombia",
-				"KM" => "Comoros (the)",
-				"180" => "Congo (the Democratic Republic of OD",
-				"CG" => "Congo (the)",
-				"CK" => "Cook Islands (the)",
-				"CR" => "Costa Rica",
-				"CI" => "Côte d'Ivoire",
-				"HR" => "Croatia",
-				"CU" => "Cuba",
-				"CW" => "Curaçao",
-				"CY" => "Cyprus",
-				"CZ" => "Czechia",
-				"DK" => "Denmark",
-				"DJ" => "Djibouti",
-				"DM" => "Dominica",
-				"DO" => "Dominican Republic (the)",
-				"EC" => "Ecuador",
-				"EG" => "Egypt",
-				"SV" => "El Salvador",
-				"GQ" => "Equatorial Guinea",
-				"ER" => "Eritrea",
-				"EE" => "Estonia",
-				"ET" => "Ethiopia",
-				"FK" => "Falkland Islands (the) [Malvinas]",
-				"FO" => "Faroe Islands (the)",
-				"FJ" => "Fiji",
-				"FI" => "Finland",
-				"FR" => "France",
-				"GF" => "French Guiana",
-				"PF" => "French Polynesia",
-				"TF" => "French Southern Territories (the)",
-				"GA" => "Gabon",
-				"GM" => "Gambia (the)",
-				"GE" => "Georgia",
-				"DE" => "Germany",
-				"GH" => "Ghana",
-				"GI" => "Gibraltar",
-				"GR" => "Greece",
-				"GL" => "Greenland",
-				"GD" => "Grenada",
-				"GP" => "Guadeloupe",
-				"GU" => "Guam",
-				"GT" => "Guatemala",
-				"GG" => "Guernsey",
-				"GN" => "Guinea",
-				"GW" => "Guinea-Bissau",
-				"GY" => "Guyana",
-				"HT" => "Haiti",
-				"HM" => "Heard Island and McDonald Islands",
-				"VA" => "Holy See (the)",
-				"HN" => "Honduras",
-				"HK" => "Hong Kong",
-				"HU" => "Hungary",
-				"IS" => "Iceland",
-				"IN" => "India",
-				"ID" => "Indonesia",
-				"IR" => "Iran (Islamic Republic of)",
-				"IQ" => "Iraq",
-				"IE" => "Ireland",
-				"IM" => "Isle of Man",
-				"IL" => "Israel",
-				"IT" => "Italy",
-				"JM" => "Jamaica",
-				"JP" => "Japan",
-				"JE" => "Jersey",
-				"JO" => "Jordan",
-				"KZ" => "Kazakhstan",
-				"KE" => "Kenya",
-				"KI" => "Kiribati",
-				"KP" => "North Korea",
-				"KR" => "South Korea",
-				"KW" => "Kuwait",
-				"KG" => "Kyrgyzstan",
-				"LA" => "Lao People's Democratic Republic (the)",
-				"LV" => "Latvia",
-				"LB" => "Lebanon",
-				"LS" => "Lesotho",
-				"LR" => "Liberia",
-				"LY" => "Libya",
-				"LI" => "Liechtenstein",
-				"LT" => "Lithuania",
-				"LU" => "Luxembourg",
-				"MO" => "Macao",
-				"MK" => "Macedonia (the former Yugoslav Republic of)",
-				"MG" => "Madagascar",
-				"MW" => "Malawi",
-				"MY" => "Malaysia",
-				"MV" => "Maldives",
-				"ML" => "Mali",
-				"MT" => "Malta",
-				"MH" => "Marshall Islands (the)",
-				"MQ" => "Martinique",
-				"MR" => "Mauritania",
-				"MU" => "Mauritius",
-				"YT" => "Mayotte",
-				"MX" => "Mexico",
-				"FM" => "Micronesia (Federated States of)",
-				"MD" => "Moldova (the Republic of)",
-				"MC" => "Monaco",
-				"MN" => "Mongolia",
-				"ME" => "Montenegro",
-				"MS" => "Montserrat",
-				"MA" => "Morocco",
-				"MZ" => "Mozambique",
-				"MM" => "Myanmar",
-				"NA" => "Namibia",
-				"NR" => "Nauru",
-				"NP" => "Nepal",
-				"NL" => "Netherlands (the)",
-				"NC" => "New Caledonia",
-				"NZ" => "New Zealand",
-				"NI" => "Nicaragua",
-				"NE" => "Niger (the)",
-				"NG" => "Nigeria",
-				"NU" => "Niue",
-				"NF" => "Norfolk Island",
-				"MP" => "Northern Mariana Islands (the)",
-				"NO" => "Norway",
-				"OM" => "Oman",
-				"PK" => "Pakistan",
-				"PW" => "Palau",
-				"PS" => "Palestine, State of",
-				"PA" => "Panama",
-				"PG" => "Papua New Guinea",
-				"PY" => "Paraguay",
-				"PE" => "Peru",
-				"PH" => "Philippines (the)",
-				"PN" => "Pitcairn",
-				"PL" => "Poland",
-				"PT" => "Portugal",
-				"PR" => "Puerto Rico",
-				"QA" => "Qatar",
-				"RE" => "Réunion",
-				"RO" => "Romania",
-				"RU" => "Russian Federation (the)",
-				"RW" => "Rwanda",
-				"BL" => "Saint Barthélemy",
-				"SH" => "Saint Helena, Ascension and Tristan da Cunha",
-				"KN" => "Saint Kitts and Nevis",
-				"LC" => "Saint Lucia",
-				"MF" => "Saint Martin (French part)",
-				"PM" => "Saint Pierre and Miquelon",
-				"VC" => "Saint Vincent and the Grenadines",
-				"WS" => "Samoa",
-				"SM" => "San Marino",
-				"ST" => "Sao Tome and Principe",
-				"SA" => "Saudi Arabia",
-				"SN" => "Senegal",
-				"RS" => "Serbia",
-				"SC" => "Seychelles",
-				"SL" => "Sierra Leone",
-				"SG" => "Singapore",
-				"SX" => "Sint Maarten (Dutch part)",
-				"SK" => "Slovakia",
-				"SI" => "Slovenia",
-				"SB" => "Solomon Islands",
-				"SO" => "Somalia",
-				"ZA" => "South Africa",
-				"GS" => "South Georgia and the South Sandwich Islands",
-				"SS" => "South Sudan",
-				"ES" => "Spain",
-				"LK" => "Sri Lanka",
-				"SD" => "Sudan (the)",
-				"SR" => "Suriname",
-				"SJ" => "Svalbard and Jan Mayen",
-				"SZ" => "Swaziland",
-				"SE" => "Sweden",
-				"CH" => "Switzerland",
-				"SY" => "Syrian Arab Republic",
-				"TW" => "Taiwan (Province of China)",
-				"TJ" => "Tajikistan",
-				"TZ" => "Tanzania, United Republic of",
-				"TH" => "Thailand",
-				"YK" => "The Republic of Kosovo",
-				"TL" => "Timor-Leste",
-				"TG" => "Togo",
-				"TK" => "Tokelau",
-				"TO" => "Tonga",
-				"TT" => "Trinidad and Tobago",
-				"TN" => "Tunisia",
-				"TR" => "Turkey",
-				"TM" => "Turkmenistan",
-				"TC" => "Turks and Caicos Islands (the)",
-				"TV" => "Tuvalu",
-				"UG" => "Uganda",
-				"UA" => "Ukraine",
-				"AE" => "United Arab Emirates(the)",
-				"GB" => "United Kingdom of Great Britain and Northern Irela",
-				"UM" => "United States Minor Outlying Islands (the)",
-				"UY" => "Uruguay",
-				"UZ" => "Uzbekistan",
-				"VU" => "Vanuatu",
-				"VE" => "Venezuela (Bolivarian Republic of)",
-				"VN" => "Viet Nam",
-				"VG" => "Virgin Islands (British)",
-				"VI" => "Virgin Islands (U.S.)",
-				"WF" => "Wallis and Futuna",
-				"EH" => "Western Sahara*",
-				"YE" => "Yemen",
-				"ZM" => "Zambia",
-				"ZW" => "Zimbabwe"
-			);
 
-
+				//$countries is stored in functions.php
 				foreach($countries as $ckey => $countryoption){
 
 					if($ckey == $country){
--- invoice.php
+++ invoice.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Revisions:  See revision notes in purchases_10.php
-/*
+*/
 ?>
 
 <style>
@@ -117,6 +117,45 @@ $ordernum = isset($_GET['ordernum'])?$_GET['ordernum']:"";
 
 
 
+
+
+include "dbconnect_prepstmt.php";
+
+
+$sql = "SELECT * FROM cjorders WHERE refnum=?";
+$stmt = $conn->prepare($sql); 
+$stmt->bind_param("s", $refnum);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+
+$data = $result->fetch_all(MYSQLI_ASSOC);
+
+
+
+$stmt->close();
+$conn->close();
+
+
+//echo "<pre>";
+//print_r($data);
+//echo "</pre>";
+
+$buyer = $data[0]['buyer'];
+$paymethod = $data[0]['paymethod'];
+$addr_bill_pieces = explode(",", $data[0]['bill_addr']);
+$orderjson = $data[0]['json'];
+
+$street_bill = $addr_bill_pieces[0];
+$city_bill = $addr_bill_pieces[1];
+$state_bill = $addr_bill_pieces[2];
+$zip_bill = $addr_bill_pieces[3];
+$country_bill = $addr_bill_pieces[4];
+
+
+
+
 $orderdate = date("F j, Y", $refnum);
 
 
@@ -125,14 +164,14 @@ $orderdate = date("F j, Y", $refnum);
 
 		$subtotal = 0;
 		$grandtotal = 0;
-		$paymethod = "";
+		
 
-		$dir = "orders/";
-		$file = $dir."order_".$refnum.".htm";
+		//$dir = "orders/";
+		//$file = $dir."order_".$refnum.".htm";
+		//$contents = file_get_contents($file);
+		//$contents = strip_tags($contents);
 
-		$contents = file_get_contents($file);
-
-		$contents = strip_tags($contents);
+		$contents = $orderjson;
 
 		//echo "$contents";
 
@@ -225,41 +264,6 @@ Print this page for your records<br><br>
 <div class="invoice-top">
 <?php
 echo "Order Date:  $orderdate<br>";
-
-
-include "dbconnect_prepstmt.php";
-
-
-$sql = "SELECT * FROM cjorders WHERE refnum=?";
-$stmt = $conn->prepare($sql); 
-$stmt->bind_param("s", $refnum);
-$stmt->execute();
-
-$result = $stmt->get_result();
-
-
-$data = $result->fetch_all(MYSQLI_ASSOC);
-
-
-
-$stmt->close();
-$conn->close();
-
-
-//echo "<pre>";
-//print_r($data);
-//echo "</pre>";
-
-$buyer = $data[0]['buyer'];
-$paymethod = $data[0]['paymethod'];
-$addr_bill_pieces = explode(",", $data[0]['bill_addr']);
-
-$street_bill = $addr_bill_pieces[0];
-$city_bill = $addr_bill_pieces[1];
-$state_bill = $addr_bill_pieces[2];
-$zip_bill = $addr_bill_pieces[3];
-$country_bill = $addr_bill_pieces[4];
-
 
 //echo "Buyer: $buyer<br>";
 
@@ -363,260 +367,6 @@ echo "$recipient<br>";
 echo "$street<br>";
 echo "$city, $state $zip<br>";
 
-
-
-			$countries = array(
-				"US" => "United States",
-				"AF" => "Afghanistan",
-				"AX" => "Åland Islands",
-				"AL" => "Albania",
-				"DZ" => "Algeria",
-				"AS" => "American Samoa",
-				"AD" => "Andorra",
-				"AO" => "Angola",
-				"AI" => "Anguilla",
-				"AQ" => "Antarctica",
-				"AG" => "Antigua and Barbuda",
-				"AR" => "Argentina",
-				"AM" => "Armenia",
-				"AW" => "Aruba",
-				"AU" => "Australia",
-				"AT" => "Austria",
-				"AZ" => "Azerbaijan",
-				"BS" => "Bahamas",
-				"BH" => "Bahrain",
-				"BD" => "Bangladesh",
-				"BB" => "Barbados",
-				"BY" => "Belarus",
-				"BE" => "Belgium",
-				"BZ" => "Belize",
-				"BJ" => "Benin",
-				"BM" => "Bermuda",
-				"BT" => "Bhutan",
-				"BO" => "Bolivia",
-				"BQ" => "Bonaire, Sint Eustatius and Saba",
-				"BA" => "Bosnia and Herzegovina",
-				"BW" => "Botswana",
-				"BV" => "Bouvet Island",
-				"BR" => "Brazil",
-				"IO" => "British Indian Ocean Territory",
-				"BN" => "Brunei Darussalam",
-				"BG" => "Bulgaria",
-				"BF" => "Burkina Faso",
-				"BI" => "Burundi",
-				"CV" => "Cabo Verde",
-				"KH" => "Cambodia",
-				"CM" => "Cameroon",
-				"CA" => "Canada",
-				"KY" => "Cayman Islands",
-				"CF" => "Central African Republic",
-				"TD" => "Chad",
-				"CL" => "Chile",
-				"CN" => "China",
-				"CX" => "Christmas Island",
-				"CC" => "Cocos (Keeling) Islands",
-				"CO" => "Colombia",
-				"KM" => "Comoros",
-				"180" => "Congo (the Democratic Republic of OD",
-				"CG" => "Congo",
-				"CK" => "Cook Islands",
-				"CR" => "Costa Rica",
-				"CI" => "Côte d'Ivoire",
-				"HR" => "Croatia",
-				"CU" => "Cuba",
-				"CW" => "Curaçao",
-				"CY" => "Cyprus",
-				"CZ" => "Czechia",
-				"DK" => "Denmark",
-				"DJ" => "Djibouti",
-				"DM" => "Dominica",
-				"DO" => "Dominican Republic",
-				"EC" => "Ecuador",
-				"EG" => "Egypt",
-				"SV" => "El Salvador",
-				"GQ" => "Equatorial Guinea",
-				"ER" => "Eritrea",
-				"EE" => "Estonia",
-				"ET" => "Ethiopia",
-				"FK" => "Falkland Islands",
-				"FO" => "Faroe Islands",
-				"FJ" => "Fiji",
-				"FI" => "Finland",
-				"FR" => "France",
-				"GF" => "French Guiana",
-				"PF" => "French Polynesia",
-				"TF" => "French Southern Territories",
-				"GA" => "Gabon",
-				"GM" => "Gambia",
-				"GE" => "Georgia",
-				"DE" => "Germany",
-				"GH" => "Ghana",
-				"GI" => "Gibraltar",
-				"GR" => "Greece",
-				"GL" => "Greenland",
-				"GD" => "Grenada",
-				"GP" => "Guadeloupe",
-				"GU" => "Guam",
-				"GT" => "Guatemala",
-				"GG" => "Guernsey",
-				"GN" => "Guinea",
-				"GW" => "Guinea-Bissau",
-				"GY" => "Guyana",
-				"HT" => "Haiti",
-				"HM" => "Heard Island and McDonald Islands",
-				"VA" => "Holy See",
-				"HN" => "Honduras",
-				"HK" => "Hong Kong",
-				"HU" => "Hungary",
-				"IS" => "Iceland",
-				"IN" => "India",
-				"ID" => "Indonesia",
-				"IR" => "Iran",
-				"IQ" => "Iraq",
-				"IE" => "Ireland",
-				"IM" => "Isle of Man",
-				"IL" => "Israel",
-				"IT" => "Italy",
-				"JM" => "Jamaica",
-				"JP" => "Japan",
-				"JE" => "Jersey",
-				"JO" => "Jordan",
-				"KZ" => "Kazakhstan",
-				"KE" => "Kenya",
-				"KI" => "Kiribati",
-				"KP" => "North Korea",
-				"KR" => "South Korea",
-				"KW" => "Kuwait",
-				"KG" => "Kyrgyzstan",
-				"LA" => "Lao People's Democratic Republic",
-				"LV" => "Latvia",
-				"LB" => "Lebanon",
-				"LS" => "Lesotho",
-				"LR" => "Liberia",
-				"LY" => "Libya",
-				"LI" => "Liechtenstein",
-				"LT" => "Lithuania",
-				"LU" => "Luxembourg",
-				"MO" => "Macao",
-				"MK" => "Macedonia",
-				"MG" => "Madagascar",
-				"MW" => "Malawi",
-				"MY" => "Malaysia",
-				"MV" => "Maldives",
-				"ML" => "Mali",
-				"MT" => "Malta",
-				"MH" => "Marshall Islands",
-				"MQ" => "Martinique",
-				"MR" => "Mauritania",
-				"MU" => "Mauritius",
-				"YT" => "Mayotte",
-				"MX" => "Mexico",
-				"FM" => "Micronesia",
-				"MD" => "Moldova",
-				"MC" => "Monaco",
-				"MN" => "Mongolia",
-				"ME" => "Montenegro",
-				"MS" => "Montserrat",
-				"MA" => "Morocco",
-				"MZ" => "Mozambique",
-				"MM" => "Myanmar",
-				"NA" => "Namibia",
-				"NR" => "Nauru",
-				"NP" => "Nepal",
-				"NL" => "Netherlands",
-				"NC" => "New Caledonia",
-				"NZ" => "New Zealand",
-				"NI" => "Nicaragua",
-				"NE" => "Niger",
-				"NG" => "Nigeria",
-				"NU" => "Niue",
-				"NF" => "Norfolk Island",
-				"MP" => "Northern Mariana Islands",
-				"NO" => "Norway",
-				"OM" => "Oman",
-				"PK" => "Pakistan",
-				"PW" => "Palau",
-				"PS" => "Palestine, State of",
-				"PA" => "Panama",
-				"PG" => "Papua New Guinea",
-				"PY" => "Paraguay",
-				"PE" => "Peru",
-				"PH" => "Philippines",
-				"PN" => "Pitcairn",
-				"PL" => "Poland",
-				"PT" => "Portugal",
-				"PR" => "Puerto Rico",
-				"QA" => "Qatar",
-				"RE" => "Réunion",
-				"RO" => "Romania",
-				"RU" => "Russian Federation",
-				"RW" => "Rwanda",
-				"BL" => "Saint Barthélemy",
-				"SH" => "Saint Helena, Ascension and Tristan da Cunha",
-				"KN" => "Saint Kitts and Nevis",
-				"LC" => "Saint Lucia",
-				"MF" => "Saint Martin (French part)",
-				"PM" => "Saint Pierre and Miquelon",
-				"VC" => "Saint Vincent and the Grenadines",
-				"WS" => "Samoa",
-				"SM" => "San Marino",
-				"ST" => "Sao Tome and Principe",
-				"SA" => "Saudi Arabia",
-				"SN" => "Senegal",
-				"RS" => "Serbia",
-				"SC" => "Seychelles",
-				"SL" => "Sierra Leone",
-				"SG" => "Singapore",
-				"SX" => "Sint Maarten (Dutch part)",
-				"SK" => "Slovakia",
-				"SI" => "Slovenia",
-				"SB" => "Solomon Islands",
-				"SO" => "Somalia",
-				"ZA" => "South Africa",
-				"GS" => "South Georgia and the South Sandwich Islands",
-				"SS" => "South Sudan",
-				"ES" => "Spain",
-				"LK" => "Sri Lanka",
-				"SD" => "Sudan",
-				"SR" => "Suriname",
-				"SJ" => "Svalbard and Jan Mayen",
-				"SZ" => "Swaziland",
-				"SE" => "Sweden",
-				"CH" => "Switzerland",
-				"SY" => "Syrian Arab Republic",
-				"TW" => "Taiwan",
-				"TJ" => "Tajikistan",
-				"TZ" => "Tanzania",
-				"TH" => "Thailand",
-				"YK" => "The Republic of Kosovo",
-				"TL" => "Timor-Leste",
-				"TG" => "Togo",
-				"TK" => "Tokelau",
-				"TO" => "Tonga",
-				"TT" => "Trinidad and Tobago",
-				"TN" => "Tunisia",
-				"TR" => "Turkey",
-				"TM" => "Turkmenistan",
-				"TC" => "Turks and Caicos Islands",
-				"TV" => "Tuvalu",
-				"UG" => "Uganda",
-				"UA" => "Ukraine",
-				"AE" => "United Arab Emirates(the)",
-				"GB" => "United Kingdom of Great Britain and Northern Irela",
-				"UM" => "United States Minor Outlying Islands",
-				"UY" => "Uruguay",
-				"UZ" => "Uzbekistan",
-				"VU" => "Vanuatu",
-				"VE" => "Venezuela",
-				"VN" => "Viet Nam",
-				"VG" => "Virgin Islands (British)",
-				"VI" => "Virgin Islands (U.S.)",
-				"WF" => "Wallis and Futuna",
-				"EH" => "Western Sahara*",
-				"YE" => "Yemen",
-				"ZM" => "Zambia",
-				"ZW" => "Zimbabwe"
-			);
 
 
 echo $countries[$country]."<br><br>";
