b094447aeecaffa21d42798dcc653dc16081f3e3
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2023-09-23 19:09:35 -0500

    Products can now be added just by SKU

--- addproduct.php
+++ addproduct.php
@@ -1,13 +1,17 @@
+<?php
+//Revisions:  Products can now be added just by entering the SKU and the markup amount.
+?>
+
 <style>
 
 .formcontainer{
   display:flex;
   margin:0 auto;
-  max-width:1000px;
+  max-width:800px;
   flex-direction:column;
   justify-content:center;
   align-items:center;
-  border:1px black solid;
+  /*border:1px black solid;*/
 }
 
 
@@ -20,83 +24,163 @@
 }
 
 .fields{
+  flex:1;
   display:flex;
   justify-content: space-between;
-  width:450px;
-  padding:10px 0px 10px;
+  width:56%;
+  padding:10px 30px 10px;
 }
 
 .jsonbox, .detailsbox{
-  height:300px;
-  min-width:177px;
+  display:flex;
+  flex:1;
+  height:100px;
+  max-width:364px;
 }
 
 
 input, textarea {
-  width:370px;
+  flex:1;
+  max-width:370px;
+}
+
+
+.addprodfieldnames{
+   display:flex;
+   justify-content:left;
+   /*border: 1px black solid;*/
+   max-width:100px;
 }
 
 </style>
 
 
+<script src="checkout.js"></script>
+
+
+<div style="text-align:center;padding:10px;background-color:#ffffff">
+<font style="font-family:BebasNeue,San-serif,Verdana,Arial;color:#000000;font-size:1.9em;">Add Product</font>
+</div>
+
+
 <form action="" method="POST" class="formcontainer">
 
-<div class="fields">Title:<input type="text" name="title"></div>
-
-<div class="fields">SKU:<input type="text" name="sku"></div>
-<div class="fields">JSON:<textarea class="jsonbox" name="json">Get json string from allvariants.php</textarea></div>
-<div class="fields">Image:<input type="text" name="image"></div>
-<div class="fields">Image Set:<input type="text" name="imageset" value="Get from curl_productdetails.php [productImage] string"></div>
-<div class="fields">Markup:<input type="text" name="markup" value="10"></div>
-<div class="fields">Details:<textarea class="detailsbox" name="details"></textarea></div>
+<div class="fields"><div class="addprodfieldnames" title="The SKU of the product you want to add from CJDropshipping">SKU:</div><input type="text" name="sku"></div>
+<div class="fields"><div class="addprodfieldnames" title="The profit you want to make">Markup:</div><input id="markup" type="text" name="markup" value="10" oninput="onlyfloat('markup', 2);"></div>
 <div class="fields">&nbsp;<input type="submit" name="submit" value="Submit"></div>
 </form>
 
 
 <div class="info">
 
+
+
 <?php
 
 if(isset($_POST['submit'])){
 
-	include "dbconnect_prepstmt.php";
+
+	if(!empty($_POST['sku']) && !empty($_POST['markup'])){
+
+		$sku = $_POST['sku'];
+		$markup = $_POST['markup'];
+
+		include "functions.php";
 
 
-	$title = $_POST['title'];
-
-	$sku = $_POST['sku'];
-	$json = $_POST['json']; 
-
-	$json = htmlspecialchars($json);//convert back using htmlspecialchars_decode()
-
-	$image = $_POST['image']; 
-	$imageset = $_POST['imageset'];
-
-	$markup = $_POST['markup'];
-	$details = $_POST['details'];
-	$details = htmlspecialchars($details);
-
-	$vieworder = "";
-
-	$query = "INSERT INTO cjproducts (title, sku, json, image, imageset, markup, details) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-	//$title, $sku, $json, $image, $imageset, $markup, $details
-
-	$stmt = $conn->prepare($query);
-	$stmt->bind_param("sssssss", $title, $sku, $json, $image, $imageset, $markup, $details);
+		//echo "<hr>";
+		$descripimages =  $decodedData['data']['description'];
 
 
-	$result = $stmt->execute();
 
-	if($result){
-		echo "Data successfully inserted<br>";
+		$descriptext = strip_tags($decodedData['data']['description'], '<p><br>');
+
+		$productImageSet = $decodedData['data']['productImageSet'];
+		$title = $decodedData['data']['productNameEn'];
+
+
+		$variants = $decodedData['data']['variants'];
+
+		$variantsjson = json_encode($variants);
+
+
+		preg_match_all("/http[:a-zA-Z0-9-\.\/]+\.com\/[a-zA-Z0-9-]+\.(jpg|png)/", $descripimages, $imagematches);
+
+
+
+		//echo "<pre>";
+		//print_r($productImageSet);
+		//print_r($imagematches[0]);
+		//echo "</pre>";
+
+		$mergedimages = array_merge($productImageSet, $imagematches[0]);
+
+		//echo "Count before unique function: ".count($mergedimages)."<br>";
+
+		$mergedimages = array_unique($mergedimages);
+
+		//echo "Count after unique function: ".count($mergedimages)."<br>";
+
+		//Insert into DB table
+		$imagestring = implode(",", $mergedimages);
+
+		$storefrontimage = $mergedimages[0];
+
+		//echo "Image set string:$imagestring<br> ";
+
+		//echo "Title: $title<br>";
+
+
+		//echo "<br><br>Desciption with images striped:<br>";
+		//echo "$descriptext<br>";
+
+
+
+
+		include "dbconnect_prepstmt.php";
+
+
+
+		$listorder = "";
+		$json = $variantsjson; 
+
+		$json = htmlspecialchars($json);//convert back using htmlspecialchars_decode()
+
+		$image = $storefrontimage; 
+		$imageset = $imagestring;
+
+
+
+		$details = htmlspecialchars($descriptext);
+
+		$vieworder = "";
+
+		$query = "INSERT INTO cjproducts (title, listorder, sku, json, image, imageset, markup, details) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+		//$title, $sku, $json, $image, $imageset, $markup, $details
+
+		$stmt = $conn->prepare($query);
+		$stmt->bind_param("ssssssss", $title, $listorder, $sku, $json, $image, $imageset, $markup, $details);
+
+
+		$result = $stmt->execute();
+
+		if($result){
+			echo "Data successfully inserted<br>";
+		}else{
+			echo "Failed to insert data<br>";
+		}
 	}else{
-		echo "Failed to insert data<br>";
-	}
+		if(empty($_POST['sku'])){
+			echo "You must enter an SKU.<br>";
+		}
 
+		if(empty($_POST['markup'])){
+			echo "You must enter a markup amount.<br>";
+		}
+	}
 }
 
 ?>
-</div>
 
-
+</div><!--class="info"-->
