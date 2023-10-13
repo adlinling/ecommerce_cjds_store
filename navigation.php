c64971c609395fe7ed6646320024c5f9db488875
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2023-10-12 23:53:57 -0500

    Refinements

--- navigation.php
+++ navigation.php
@@ -6,11 +6,22 @@ $indexpage = $_SERVER['PHP_SELF'];
 <div id="navigation">
 
 
+<form name="search" class="searchform" action="index.php?pg=search" method="POST" ">
+<input type="text" name="search" class="search-field" placeholder="Search">
+<button name="submit" class="search-button"><img src="magnifier.png"></button>
+</form>
 
 	<div class="nav"><a href="<?php echo $indexpage;?>"><font class="navigation">Home</font></a></div>
 	<div class="nav"><a href="<?php echo $indexpage;?>?pg=store"><font class="navigation">Store</font></a></div>
-	<div class="nav"><a href="<?php echo $indexpage;?>?pg=viewcart"><font class="navigation">Cart</font></a></div>
 	<div class="nav"><a href="<?php echo $indexpage;?>?pg=contact"><font class="navigation">Contact</font></a></div>
+
+
+
+	<!--
+
+	<div class="nav"><a href="<?php echo $indexpage;?>?pg=tutorials">Tutorials</a></div>
+	<div class="nav"><a href="<?php echo $indexpage;?>?pg=links">Links</a></div>
+	-->
 
 
 
@@ -27,7 +38,7 @@ if(isset($_COOKIE['session_id']) || isset($_SESSION['session_id'])){
 ?>
 
 	<div class="nav"><a href="<?php echo $indexpage;?>?pg=account"><font class="navigation">Account</a></font></div>
-	<div class="nav"><a href="<?php echo $indexpage;?>?pg=logout"><font class="navigation">Log Out</a></font></div>
+	<div class="nav"><a href="<?php echo $indexpage;?>?pg=logout"><font class="navigation">Log&nbsp;out</a></font></div>
 
 
 <?php
@@ -36,13 +47,12 @@ if(isset($_COOKIE['session_id']) || isset($_SESSION['session_id'])){
 
 ?>
 	<div class="nav"><a href="<?php echo $indexpage;?>?pg=register"><font class="navigation">Register</a></font></div>
-	<div class="nav"><a href="<?php echo $indexpage;?>?pg=login"><font class="navigation">Log In</a></font></div>
-
+	<div class="nav"><a href="<?php echo $indexpage;?>?pg=login"><font class="navigation">Log&nbsp;In</a></font></div>
 
 <?php
 }
 ?>
 
-
+	<div class="carticon" title="Cart"><a href="<?php echo $indexpage;?>?pg=viewcart"><image class="cart" src="cart.png"></a></div>
 
 </div>
