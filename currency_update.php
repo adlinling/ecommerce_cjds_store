b916eb97588bec572818ccdc2dc223a6c0aab021
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2023-11-15 23:55:20 -0600

    Refinements

--- currency_update.php
+++ currency_update.php
@@ -1,4 +1,15 @@
 <?php
+//Run currency_trigger_update.php in localhost to trigger this script on a remote server
+
+date_default_timezone_set('America/Chicago');
+
+$timeofpost = time();
+
+$timeofpost = date("m-d-Y h:i:s", $timeofpost)." CT";
+
+
+$string = "currency_update.php received post at ".$timeofpost."<br/>";
+
 
 $timenow = time();
 $rowID = "1";
@@ -16,10 +27,10 @@ $data = $result->fetch_all(MYSQLI_ASSOC);
 
 $stmt->close();
 
-echo "Old exchange rates";
-echo "<pre>";
-print_r($data);
-echo "</pre>";
+$string .= "Old exchange rates";
+$string .= "<pre>";
+$string .= json_encode($data, JSON_PRETTY_PRINT);
+$string .= "</pre>";
 
 $lastupdate = $data[0]['lastupdate'];
 $timelapsed = $timenow - $lastupdate;
@@ -29,50 +40,55 @@ $oneday = $onehour*24;
 
 $hourslapsed = $timelapsed/$onehour;
 
-echo "Time of last update: $lastupdate<br>";
-echo "Hours since last update: $hourslapsed<br>";
+$string .= "Time of last update: $lastupdate<br>";
+$string .= "Hours since last update: $hourslapsed<br>";
 
-$frequency = 24;//hours
+$frequency = isset($_GET['freq'])?$_GET['freq']:24;//hours
+//$frequency = 0.00166;//hours
+
+echo "<a href='$thisfile?freq=1'>Update Now</a><br><br>";
+
 
 if($hourslapsed > $frequency){
+//if(true){
 
-
-	echo "Time to update exchange rates.<br>";
-
-
+	$string .= "Time to update exchange rates.<br>";
 
 	//See currencylayercom.php for login credentials for the API account
 
 	// set API Endpoint and access key (and any options of your choice)
 	$endpoint = 'live';
-	$access_key = 'YOUR API KEY';
-	$currencies = "CAD,AUD,GBP,EUR,JPY,INR,PKR,NZD";
+	$access_key = '[your API key]';
+	$currencies = "CAD,AUD,GBP,EUR,JPY,INR,PKR,NZD";//Paypal does not support PKR https://developer.paypal.com/docs/reports/reference/paypal-supported-currencies/
+
 
 	$url = "http://apilayer.net/api/live?access_key=$access_key&currencies=$currencies&source=USD&format=1";
 
-	
+
+	//API code start 
 	// Initialize CURL:
-	//$ch = curl_init('https://api.currencylayer.com/'.$endpoint.'?access_key='.$access_key.'');
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 	// Store the data:
 	$json = curl_exec($ch);
 	curl_close($ch);
+
 	
 	// Decode JSON response:
 	$exchRates = json_decode($json, true);
 
 	// Access the exchange rate values, e.g. GBP:
-	//echo $exchRates['quotes']['USDGBP'];
+	//$string .= $exchRates['quotes']['USDGBP'];
 
-	echo "<pre>";
-	print_r($exchRates);
-	echo "</pre>";
+	
+	$string .= "<pre>";
+	$string .= $json;
+	$string .= "</pre>";
 
 	$success = $exchRates['success'];
 
-	echo "Success: $success<br>";
+	$string .= "Success: $success<br>";
 
 	$exchangerates = array("USD:1");
 
@@ -82,17 +98,23 @@ if($hourslapsed > $frequency){
 		$exchangerates[] = $str;
 	}
 
+	//API code end
 
-	echo "<pre>";
-	print_r($exchangerates);
-	echo "</pre>";
 
-	//This is for testing
+
+	//This is for testing.  Comment out the API code above when using it.
+	//$success = 1;
 	//$exchangerates = array("USD:1","CAD:1.".rand(0,300),"AUD:1.".rand(0,300),"GBP:0.".rand(0,300),"EUR:0.".rand(0,300),"JPY:".rand(0,300),"INR:".rand(0,300),"PKR:".rand(0,300),"NZD:".rand(0,300),);
-		
-	$json = json_encode($exchangerates);
+	
 
-	echo $json."<br>";
+	
+
+	$json = json_encode($exchangerates, JSON_PRETTY_PRINT);
+
+
+	$string .= "<pre>";
+	$string .= $json;
+	$string .= "</pre>";
 
 
 	if($success){
@@ -110,18 +132,18 @@ if($hourslapsed > $frequency){
 		}
 
 		$affectedRows = $stmt->affected_rows;
-		echo "Number of affected rows: " . $affectedRows;
+		$string .= "Number of affected rows: " . $affectedRows;
 		$stmt->close();
 	}
 
 
 }else{
-	echo "Not time to update yet";
+	$string .= "Not time to update yet";
 }
 
 
 $conn->close();
 
-
+file_put_contents("debugging_currency_update.htm", $string, FILE_APPEND);
 
 ?>
